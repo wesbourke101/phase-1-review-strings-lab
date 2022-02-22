1d4cdc6b7a2dee8229d364a4c9b40cc3b1a377de
Wes Bourke <wesbourkeavl@gmail.com>
2022-02-22 12:56:20 -0500

    commit tree 4cf99fc56198ffe45e42692f3e84324fef1d75e0

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+
+const currentUser = "Wes"
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+const shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
